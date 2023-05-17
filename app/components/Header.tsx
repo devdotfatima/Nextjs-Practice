48e0985b1cf68d4f42bc2709d0750205d8e8282e
Fatima Ali <fatimaaliforwork@gmail.com>
2023-05-17 14:59:17 +0500

    integrates mui styling in app

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,32 @@
-import Link from 'next/link';
+"use client";
+
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 
 const Header = () => {
   return (
-    <header className='header'>
-      <div className='container'>
-        <div className='logo'>
-          <Link href='/'>NextJs</Link>
-        </div>
-        <div className='links'>
-          <Link href='/reposCSR'>Repository CSR</Link>
-          <Link href='/repos'>Repos SSR</Link>
-        </div>
-      </div>
-    </header>
+    <AppBar position="static">
+      <Toolbar>
+       
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link  href="/">
+            NEXT
+            </Link>
+          </Typography>
+      
+        <Button color="inherit" href="/reposCSR">
+          Repository CSR
+        </Button>
+        <Button color="inherit" href="/repos">
+          Repos SSR
+        </Button>
+        <Button variant="contained" color="error" onClick={()=>signOut()}>
+          Sign Out
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,18 @@
+"use client"
 import './globals.css';
 import { Poppins } from 'next/font/google';
 import Header from './components/Header';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { AppProps } from 'next/app';
+import theme  from './theme';
+import createEmotionCache from './createEmotionCache';
 
+const clientSideEmotionCache = createEmotionCache();
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
 const poppins = Poppins({
   weight: ['400', '700'],
   subsets: ['latin'],
@@ -14,10 +25,15 @@ export const metadata = {
 export default function RootLayout({children,}: {children: React.ReactNode}) {
   return (
     <html lang='en'>
-    <body className={poppins.className}>
-      <Header />
-      <main className='container'>{children}</main>
-    </body>
+      <CacheProvider  value={clientSideEmotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <body className={poppins.className}>
+          <Header />
+          <main className='container'>{children}</main>
+        </body>
+        </ThemeProvider>
+    </CacheProvider>
   </html>
   )
 }
