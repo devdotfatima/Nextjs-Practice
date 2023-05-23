6976fc2f9e5338cd8634271c057a50bd42f2bd70
Fatima Ali <fatimaaliforwork@gmail.com>
2023-05-23 17:47:59 +0500

    make feature section  reponsive for mobile screens

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Features from "./components/Features";
 import Hero from "./components/Hero";
+import Testimonial from "./components/Testimonial";
 
 const HomePage = async () => {
   return (
     <div className="min-h-screen dark:bg-white text-black ">
       <Hero />
       <Features />
+      <Testimonial />
     </div>
   );
 };
