1aa4f602d9661add700c5927d0ddb19a93d970c8
Fatima Ali <fatimaaliforwork@gmail.com>
2023-06-02 15:10:49 +0500

    changes in the response of register API

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,36 +1,36 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import bcrypt from "bcrypt";
-
 import prisma from "@/app/libs/prismadb";
 
-export async function POST(  request: Request, ) {
-  try{
+export async function POST(request: NextRequest) {
+  try {
     const body = await request.json();
-    const { 
-      email,
-      name,
-      password,
-     } = body;
-     const userExists = await prisma.user.findUnique({
+    const { email, name, password } = body;
+    const userExists = await prisma.user.findUnique({
       where: {
-        email: email
-      }
+        email: email,
+      },
     });
 
     if (userExists) {
-      
-    return NextResponse.json({ error: 'This email already exists' }, { status: 500 });
+      return NextResponse.json(
+        { error: "This email already exists" },
+        { status: 409 }
+      );
     }
-     const hashedPassword = await bcrypt.hash(password, 12);
-     const user = await prisma.user.create({
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await prisma.user.create({
       data: {
         email,
         name,
         hashedPassword,
-      }
+      },
     });
-    return NextResponse.json(user);
-  }catch(err){
-    return NextResponse.json({ error: err }, { status: 500 });
+    return NextResponse.json(user, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json(error.message, {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
